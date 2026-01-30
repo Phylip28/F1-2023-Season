bd7f69ce97dcefe10db582b2dd79d66221d4ca2f
Juan Felipe Rendon Herrera <felipe28769@gmail.com>
2026-01-30 16:52:48 -0500

    Feat/build basic frontend (#6)
    * feat(front): build basic frontend
    
    * fix(front): move circuits folder into frontend and fix paths
    
    * feat(back): add cors for allow frontend request
    
    * feat(front): add loading animation and image regarding

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 from app.routers import season
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(season.router)
+app.add_middleware(
+    CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
+)
+
 
 @app.get("/")
 def health_check():
